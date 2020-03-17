98f33bde16134a335c87016b93983dc187ab103c
HirokiWindowsPC <hiroki123naka4ma@gmail.com>
2020-03-17 21:58:39 +0900

    create box

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-import {} from 'three';
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera,
+  BoxGeometry,
+  MeshStandardMaterial,
+  Mesh,
+  DirectionalLight
+} from 'three';
+
+window.addEventListener('DOMContentLoaded', init)
+
+function init() {
+  // キャンバスのサイズ指定
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  // レンダラーの作成
+  const renderer = new WebGLRenderer({
+    canvas: document.querySelector('#mycanvas')
+  });
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(width, height)
+
+  // シーンの作成
+  const scene = new Scene();
+
+  // カメラの作成
+  const camera = new PerspectiveCamera(50, width / height);
+  camera.position.set(0, 0, 1000)
+
+  // オブジェクトの作成
+  const geometry = new BoxGeometry(300, 300, 300);
+  const material = new MeshStandardMaterial({
+    color: 0x33FF00
+  });
+  const box = new Mesh(geometry, material);
+
+  // シーンに追加
+  scene.add(box);
+
+  // ライトの作成
+  const light = new DirectionalLight(0xFFFFFF, 1);
+  light.position.set(1, 1, 1);
+  scene.add(light);
+
+
+  // 初期化
+  tick();
+
+  function tick() {
+    box.rotation.x += 0.005;
+    box.rotation.y += 0.005;
+
+    // 描画
+    renderer.render(scene, camera);
+
+    // animation
+    requestAnimationFrame(tick)
+  }
+}
